7b713a1e2577b8b072314a52f2fdb307785b1a17
lolenko <lolenko@finch-melrose.com>
2014-03-08 00:43:43 +0400

    завёл реквайр

---
+++ src/ts/main.ts
@@ -0,0 +1,10 @@
+/*require.config({
+
+});*/
+///<reference path="js/libs/jquery.d.ts" />
+
+import jquery = require("libs/bower_components/jquery/dist/jquery");
+import Sticker = require('Sticker');
+
+
+new Sticker();
