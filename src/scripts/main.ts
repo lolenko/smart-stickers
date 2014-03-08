a5243a676f82098b7f7e96ca1b8e7ab184c19eca
lolenko <lolenko@finch-melrose.com>
2014-03-08 14:27:45 +0400

    убрал bower нафиг

---
+++ src/scripts/main.ts
@@ -0,0 +1,10 @@
+/*require.config({
+
+});*/
+///<reference path="../../js/libs/jquery.d.ts" />
+
+import jquery = require('../../js/libs/bower_components/jquery/dist/jquery.js');
+import Sticker = require('Sticker');
+
+$.noop();
+new Sticker();
