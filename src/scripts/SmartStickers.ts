53ebc572c36970169d5bc7582c2284988b6d63f9
lolenko <lolenko@finch-melrose.com>
2014-03-18 22:47:37 +0400

    прикидываем апи смарт стикера, пока инициализируемся через него

--- src/scripts/SmartStickers.ts
+++ src/scripts/SmartStickers.ts
@@ -1,5 +1,34 @@
-class SmartStickers {
+import jquery = require('vendor/jquery');
+import Sticker = require('./Sticker');
+if (jquery);
+var $window = $(window);
 
+var $window = $(window);
+
+class SmartStickers {
+    private stickers:Sticker[] = [];
+
+    constructor() {
+        var $stickers = $('.sticker');
+        $stickers.each((i, el) => {
+            this.register(new Sticker(el));
+        });
+        $window.on('scroll', this.onScroll.bind(this));
+    }
+
+    private onScroll() {
+        this.reposition($window.scrollTop());
+    }
+
+    private register(sticker:Sticker) {
+        this.stickers.push(sticker);
+    }
+
+    private reposition(top) {
+        this.stickers.forEach((sticker:Sticker) => {
+            sticker.reposition(top);
+        });
+    }
 }
 
 export = SmartStickers;
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,19 +1,8 @@
 ///<reference path="vendor/jquery.d.ts" />
 
 import jquery = require('vendor/jquery');
-import Sticker = require('Sticker');
+import SmartStickers = require('SmartStickers');
 
 if (jquery) $.noop();
 
-var stickers = [];
-
-$('.sticker').each(function(i, el) {
-    stickers.push(new Sticker(el));
-});
-
-$(window).on('scroll', function() {
-    var scrollTop = $(window).scrollTop();
-    stickers.forEach(function(sticker) {
-        sticker.reposition(scrollTop);
-    });
-})
+new SmartStickers();
